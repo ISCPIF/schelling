818d89ad07ce04d51a14808424faa4f37088b0c4
JusteRaimbault <raimbaultjwin@gmail.com>
2015-08-06 17:53:56 +0200

    Implemented slow indicators through fast convolution

--- scala/model/src/main/scala/fr/iscpif/spacematters/model/metric/package.scala
+++ scala/model/src/main/scala/fr/iscpif/spacematters/model/metric/package.scala
@@ -190,4 +190,58 @@ package object metric {
       if i != oi || j != oj
     } yield state(i + oi)(j + oj)
 
+
+
+
+  /**
+   * Moran index using fast convolution.
+   *
+   * @param matrix
+   * @return
+   */
+  def moran_convol(matrix: Matrix[T],quantity: Quantity[T]): Double = {
+    val conf = matrix.map { row => row.map(quantity).toArray }.toArray
+    val n = conf.length
+    val flatConf = conf.flatten
+    val popMean = flatConf.sum / flatConf.length
+    val centeredConf = conf.map { r => r.map { d => d - popMean } }
+    val variance = MathArrays.ebeMultiply(centeredConf.flatten, centeredConf.flatten).sum
+    val weights = spatialWeights(2 * n - 1)
+    val totWeight = Convolution.convolution2D(Array.fill(n, n) { 1.0 }, weights).flatten.sum
+    flatConf.length / (totWeight * variance) * MathArrays.ebeMultiply(centeredConf.flatten, Convolution.convolution2D(centeredConf, weights).flatten).sum
+  }
+
+  def spatialWeights(n: Int): Array[Array[Double]] = {
+    Array.tabulate(n, n) { (i, j) => if (i == n / 2 && j == n / 2) 0.0 else 1 / Math.sqrt((i - n / 2) * (i - n / 2) + (j - n / 2) * (j - n / 2)) }
+  }
+
+  /**
+   * Mean distance using fast convolution.
+   *
+   * @param matrix
+   * @return
+   */
+  def distance_convol(matrix: Matrix[T],quantity: Quantity[T]): Double = {
+    val conf = matrix.map { row => row.map(quantity).toArray }.toArray
+    val totPop = conf.flatten.sum
+    val dmat = distanceMatrix(2 * conf.length - 1)
+    val conv = Convolution.convolution2D(conf, dmat)
+    math.sqrt(math.Pi) / (conf.length * totPop * totPop) * MathArrays.ebeMultiply(conv.flatten, conf.flatten).sum
+  }
+
+  /**
+   * Distance kernel
+   *
+   * @param n
+   * @return
+   */
+  def distanceMatrix(n: Int): Array[Array[Double]] = {
+    Array.tabulate(n, n) { (i, j) => Math.sqrt((i - n / 2) * (i - n / 2) + (j - n / 2) * (j - n / 2)) }
+  }
+
+
+
+
+
+
 }
