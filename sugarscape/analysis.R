f1cc6a6ea33ae5f0396b4d01b4a226e55196f6e6
JusteRaimbault <raimbaultjwin@gmail.com>
2017-08-04 13:39:44 +0800

    test_schelling_metapd

--- sugarscape/analysis.R
+++ sugarscape/analysis.R
@@ -2,7 +2,7 @@
 library(dplyr)
 library(ggplot2)
 
-setwd(paste0(Sys.getenv('CS_HOME'),'/SpaceMatters/spacematters/sugarscape'))
+setwd(paste0(Sys.getenv('CS_HOME'),'/SpaceMatters/Models/spacematters/sugarscape'))
 stdtheme= theme(axis.title = element_text(size = 22), 
                 axis.text.x = element_text(size = 15),axis.text.y = element_text(size = 15),
                 strip.text = element_text(size=15),
@@ -66,23 +66,14 @@ g+geom_raster()+facet_grid(binpc1~binpc2)
 #####
 # Meta phase diagram with EMD
 
-library(emdist)
+source(paste0(Sys.getenv('CS_HOME'),'/SpaceMatters/Models/spacematters/Scripts/functions.R'))
 
 sres <- as.tbl(read.csv(file='exploration/20170328_gridsynth_sum.csv'))
 
 ref <- as.tbl(read.csv('exploration/2017_03_28_11_57_40_GRID_FIXED.csv'))
 sref = ref %>% group_by(population,minSugar,maxSugar)%>%summarise(gini=mean(mwgini))
 
-dists=c()
-for(id in unique(sres$id)){
-  show(id)
-  #dists=append(dists,emd(as.matrix(sref[,c(4,1:3)]),as.matrix(sres[sres$id==id,c(6,3:5)])))
-  #show(length(sres$gini[sres$id==id]))
-  d=left_join(sref,sres[sres$id==id,],by=c('population'='population','minSugar'='minSugar','maxSugar'='maxSugar'))
-  #dists=append(dists,2*(sd(d$gini.x-d$gini.y,na.rm = T)^2)/(sd(d$gini.x)^2+sd(d$gini.y,na.rm = T)^2))
-  dists=append(dists,2*(sum((d$gini.x-d$gini.y)^2,na.rm = T)/min(length(which(!is.na(d$gini.x))),length(which(!is.na(d$gini.y)))))/(sd(d$gini.x)^2+sd(d$gini.y,na.rm = T)^2))
-}
-names(dists)=unique(sres$id)
+dists=distancesToRef(simresults=sres,reference=sref,parameters=c('population','minSugar','maxSugar'),idcol="id")
   
 morph <- as.tbl(read.csv('exploration/20170328_gridsynth_morpho.csv'))
 names(morph)[2]<-"id"
@@ -99,37 +90,6 @@ g=ggplot(metaphase,aes(x=binpc1,y=binpc2,fill=emd))
 g+geom_raster()
 ggsave(file='res/emd_raster.png',width=12,height=10,units = 'cm')
 
-
-##
-# position of file setup within morpho space
-#moran = 0.2563323814971014"
-#observer: "entropy = 0.9640148151339423"
-#observer: "slope = -0.3742297460724673"
-#observer: "slope-r2 = 0.6328386256319237"
-#observer: "distance = 0.8290054369272968"
-rot<-read.csv('20170328_gridsynth_rotation.csv',row.names = 1 )
-matrix(data=c(0.8290054369272968,0.9640148151339423,0.2563323814971014,-0.3742297460724673),nrow=1)%*%as.matrix(rot)
-
-load('pca.RData')
-
-# plots
-
-g = ggplot(data.frame(morph,distance=dists),aes(x=PC1,y=PC2,color=distance))
-g+geom_point(size=2)+geom_point(x=1.130869,y=0.09586869,col='red',size=3)+
-  geom_point(x=morph$PC1[morph$id==27],y=morph$PC2[morph$id==27],pch=0,size=4,col=3)+
-  geom_point(x=morph$PC1[morph$id==0],y=morph$PC2[morph$id==0],pch=0,size=4,col=4)+stdtheme
-ggsave(file='res/relativedistance_morphspace.pdf',width=18,height=15,units = 'cm')
-
-g=ggplot(data.frame(metaparams,distance=dists),aes(x=alpha,y=diffusion,color=distance))
-g+geom_point(size=2)+stdtheme
-ggsave(file='res/relativedistance_metaparams.pdf',width=18,height=15,units = 'cm')
-
-
-g=ggplot(data.frame(metaparams,dist=dists),aes(x=growth,y=diffusion,color=dist))
-g+geom_point(size=2)
-
-g=ggplot(data.frame(metaparams,dist=dists),aes(x=diffSteps,y=diffusion,color=dist))
-g+geom_point(size=2)
 
 
 ### plot typical phase diagrams
