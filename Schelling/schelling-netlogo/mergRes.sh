c03793d130d31beb9625f65d4bd18cc3492794cb
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-02-11 11:18:56 +0100

    uncommited_modifs_mergeres

--- Schelling/schelling-netlogo/mergRes.sh
+++ Schelling/schelling-netlogo/mergRes.sh
@@ -1,5 +1,6 @@
 # header
-ls exploration | grep "SOBOL_GRIDS" | head -n 1 | awk '{print "head -n 1 "$0" > SOBOL_ALLGRIDS.csv"}'| sh
+cd exploration
+ls | grep "SOBOL_GRIDS" | head -n 1 | awk '{print "head -n 1 "$0" >> SOBOL_ALLGRIDS.csv"}'| sh
 # all files
-ls exploration | grep "SOBOL_GRIDS" | awk '{print "tail -n +2 "$0" > SOBOL_ALLGRIDS.csv"}'| sh
+ls | grep "SOBOL_GRIDS" | awk '{print "tail -n +2 "$0" >> SOBOL_ALLGRIDS.csv"}'| sh
 
